17d016a508224b221af2d80d853ae20624aa9442
JGarcia106 <joelitogarcia696@gmail.com>
2025-02-21 02:26:57 -0600

    Practica-1 multiplicar 2 doubles

---
+++ multiplicar2double/src/multiplicar2double/Multiplicar2double.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package multiplicar2double;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Joel
+ */
+public class Multiplicar2double {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double num1 = 5.0; // Primera variable
+        double num2 = 3.0; // Segunda variable
+        double multi = num1 * num2; // Multiplicación de las dos variables
+        JOptionPane.showMessageDialog(null, "La suma de ambos numeros es: " + multi);
+    }
+    
+}
