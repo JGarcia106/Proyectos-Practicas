7fa78fe37b001e468d92154692ed094134c52d34
JGarcia106 <joelitogarcia696@gmail.com>
2025-02-21 02:27:55 -0600

    Practica-1 Suma básica de java

---
+++ sumajava/src/sumajava/Sumajava.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sumajava;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Joel
+ */
+public class Sumajava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int num1 = 10; // Declaración y asignación de la primera variable
+        int num2 = 20; // Declaración y asignación de la segunda variable
+        int suma = num1 + num2; // Suma de las dos variables
+        JOptionPane.showMessageDialog(null,"El resultado de la suma es: "+suma);
+    }
+    
+}
