8ff0be2779f7f43c9f922a606a570d7ec2605ca1
JGarcia106 <joelitogarcia696@gmail.com>
2025-02-21 02:25:45 -0600

    Quinta clase.

---
+++ 2025-quinta-clase/src/pkg2025/quinta/clase/Carrera.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkg2025.quinta.clase;
+
+/**
+ *
+ * @author Joel
+ */
+class Carrera {
+    public String nombreCarrera;
+    public boolean acreditada;
+    public Universidad universidad;
+}
