c80e132b77aaff2220d7647f9c559c003790dbeb
JGarcia106 <joelitogarcia696@gmail.com>
2025-02-21 02:27:31 -0600

    Practica-1 Suma de int y double

---
+++ sumadeintydouble/src/sumadeintydouble/Sumadeintydouble.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sumadeintydouble;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Joel
+ */
+public class Sumadeintydouble {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int entero = 6; // Variable int
+        double decimal = 9.5; // Variable double
+        double suma = entero + decimal; // Suma de la variable int y la double
+        JOptionPane.showMessageDialog(null,"El resultado de la suma es: "+suma);
+    }
+    
+}
